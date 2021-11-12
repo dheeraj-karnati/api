c6700091c667af884770ec2302fb65a1fb5543ab
Srini Maddineni <maddinenisri@gmail.com>
2021-11-12 17:48:23 -0500

    Added logic

--- src/main/java/com/ekinsol/challenge/apiservice/controller/FileController.java
+++ src/main/java/com/ekinsol/challenge/apiservice/controller/FileController.java
@@ -2,7 +2,6 @@ package com.ekinsol.challenge.apiservice.controller;
 
 import com.ekinsol.challenge.apiservice.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,23 +14,6 @@ public class FileController {
 
     @Autowired
     FileService fileService;
-
-    @Value("${ekinsol.s3.bucketName}")
-    private String s3BucketName;
-
-    @Value("${ekinsol.s3.objectKey}")
-    private String objectKey;
-
-    private static final String FILE_MIME_TYPE_KEY = "fileMimeType";
-
-    private static final String FILE_MIME_TYPE_EXCEL = "application/vnd.ms-excel";
-
-    @GetMapping("/presignedUrl")
-    public ResponseEntity<String> getPresignedUrl() {
-
-        return new ResponseEntity<>(fileService.generatePresignedUrl(s3BucketName,objectKey), HttpStatus.OK);
-
-    }
 
     @PostMapping(value= "/upload")
     public ResponseEntity<String> uploadFile(@RequestPart(value= "file") final MultipartFile multipartFile) {
--- src/test/java/com/ekinsol/challenge/apiservice/ApiServiceApplicationTests.java
+++ src/test/java/com/ekinsol/challenge/apiservice/ApiServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ekinsol.challenge.apiservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ApiServiceApplicationTests {
 
 	@Test
