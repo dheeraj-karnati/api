16d75c85cdd3e11dc1548771c3f5596e388a8147
Srini Maddineni <maddinenisri@gmail.com>
2021-11-12 14:03:20 -0500

    Added missing properties

--- src/main/resources/db/migration/V1__create_prices.sql
+++ src/main/resources/db/migration/V1__create_prices.sql
@@ -1,3 +1,2 @@
 CREATE SEQUENCE lcat_seq;
-CREATE TABLE lcat_price (id integer NOT NULL DEFAULT nextval('lcat_seq'));
-ALTER SEQUENCE lcat_seq OWNED BY lcat_price.id;
+CREATE TABLE lcat_price(id BIGSERIAL PRIMARY KEY);
